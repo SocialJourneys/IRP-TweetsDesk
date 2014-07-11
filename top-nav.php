cf52cefe638c85a0364d2f3852f927b90b57cb6d
Mujtaba Mehdi <mujtaba@MBP.local>
2014-07-12 00:29:55 +0100

    show username

--- top-nav.php
+++ top-nav.php
@@ -13,7 +13,7 @@
             <ul class="nav navbar-top-links navbar-right">
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-gear fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                        <span style="color:black;">Welcome <strong><?php echo $_SESSION['account']['name'];?> !</strong></span><i class="fa fa-gear fa-fw"></i> <i class="fa fa-caret-down"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-user">
                         <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
